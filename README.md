1e41018058a6587c6ce92ca10117d057dbe40e8c
Toby Larone <10854475+Toby-Larone@users.noreply.github.com>
2020-05-11 21:18:31 +0100

    [meta] Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bertrand
+
+Bertrand is a discord bot built of The Programmer's Hangout that creates sentences based on it's users.
+
+## Features
+
+
+## Setup
+
---
+++ src/main/java/uk/co/markg/bertrand/App.java
@@ -0,0 +1,11 @@
+package uk.co.markg.bertrand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
