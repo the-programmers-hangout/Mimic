559a0d22234e815d44167e9a9d0d4da44b923749
Toby Larone <10854475+Toby-Larone@users.noreply.github.com>
2020-05-11 21:27:22 +0100

    [db] Add flyway migration config

--- src/main/java/uk/co/markg/bertrand/App.java
+++ src/main/java/uk/co/markg/bertrand/App.java
@@ -1,11 +1,19 @@
 package uk.co.markg.bertrand;
 
+import org.flywaydb.core.Flyway;
+
 /**
  * Hello world!
  *
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    initDatabase();
+  }
+
+  private static void initDatabase() {
+    Flyway.configure()
+        .dataSource(System.getenv("B_HOST"), System.getenv("B_USER"), System.getenv("B_PASS"))
+        .load().migrate();
   }
 }
