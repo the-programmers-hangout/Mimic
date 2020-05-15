2a67a54a180fd1aa370ed1ba977c794476b79e93
Toby Larone <10854475+Toby-Larone@users.noreply.github.com>
2020-05-15 17:37:22 +0100

    [command] Replace channe list output with embed

--- src/main/java/uk/co/markg/bertrand/command/ListChannels.java
+++ src/main/java/uk/co/markg/bertrand/command/ListChannels.java
@@ -1,7 +1,10 @@
 package uk.co.markg.bertrand.command;
 
+import java.awt.Color;
 import java.util.List;
 import disparse.parser.reflection.CommandHandler;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import uk.co.markg.bertrand.database.ChannelRepository;
 import uk.co.markg.bertrand.db.tables.pojos.Channels;
@@ -23,18 +26,19 @@ public class ListChannels {
 
   private void execute() {
     var channels = channelRepo.getAll();
-    String message = buildListOfChannels(channels);
+    var message = buildListOfChannels(channels);
     event.getChannel().sendMessage(message).queue();
   }
 
-  private String buildListOfChannels(List<Channels> channels) {
-    StringBuilder message = new StringBuilder();
-    message.append("channel | read | write").append(System.lineSeparator());
+  private MessageEmbed buildListOfChannels(List<Channels> channels) {
+    EmbedBuilder eb = new EmbedBuilder();
+    eb.setTitle("Channels");
+    eb.setColor(Color.decode("#eb7701"));
     for (Channels channel : channels) {
-      message.append("<#").append(channel.getChannelid());
-      message.append("> | ").append(channel.getReadPerm()).append(" | ")
-          .append(channel.getWritePerm()).append(System.lineSeparator());
+      eb.addField("Channel", "<#" + channel.getChannelid() + ">", true);
+      eb.addField("Read", channel.getReadPerm().toString(), true);
+      eb.addField("Write", channel.getWritePerm().toString(), true);
     }
-    return message.toString();
+    return eb.build();
   }
 }
