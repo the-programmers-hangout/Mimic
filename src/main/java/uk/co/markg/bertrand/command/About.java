ef1c4d1c4f9d47738fe75f9a8b417d69aa089f8c
Jared Bosco <boscojared@gmail.com>
2020-06-20 13:50:19 -0400

    [commands] Update about command to use disparse request / response

--- src/main/java/uk/co/markg/bertrand/command/About.java
+++ src/main/java/uk/co/markg/bertrand/command/About.java
@@ -1,5 +1,7 @@
 package uk.co.markg.bertrand.command;
 
+import disparse.discord.jda.DiscordRequest;
+import disparse.discord.jda.DiscordResponse;
 import disparse.parser.reflection.CommandHandler;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -8,15 +10,15 @@ import java.awt.Color;
 public class About {
 
   @CommandHandler(commandName = "about", description = "Displays info about the bot")
-  public static void execute(MessageReceivedEvent event) {
+  public static DiscordResponse execute(DiscordRequest request) {
     EmbedBuilder eb = new EmbedBuilder();
     eb.setTitle("Mimic Bot");
     eb.setDescription("Mimic is a bot that generates messages using markov chains based on user messages. "
         + "Opting in will tell mimic to read the last 50,000 messages per configured channel and save your messages. "
         + "It will then read any new valid messages sent in those channels.");
-    eb.setThumbnail(event.getJDA().getSelfUser().getAvatarUrl());
+    eb.setThumbnail(request.getEvent().getJDA().getSelfUser().getAvatarUrl());
     eb.addField("Source", "https://github.com/Toby-Larone/bertrand", false);
     eb.setColor(Color.decode("#eb7701"));
-    event.getChannel().sendMessage(eb.build()).queue();
+    return DiscordResponse.of(eb);
   }
 }
