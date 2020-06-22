5985ab75963d243b5f41d696467a9cd76dd7e066
Toby Larone <10854475+Toby-Larone@users.noreply.github.com>
2020-06-22 23:14:40 +0100

    [command] Fix bug causing non-candidates to still generate messages

--- src/main/java/uk/co/markg/bertrand/command/markov/MarkovSelf.java
+++ src/main/java/uk/co/markg/bertrand/command/markov/MarkovSelf.java
@@ -13,7 +13,7 @@ public class MarkovSelf {
   @CommandHandler(commandName = "self",
       description = "Generate a random number of sentences from your own messages!")
   public static void execute(DiscordRequest request, ChannelRepository channelRepo,
-                             UserRepository userRepo) {
+      UserRepository userRepo) {
     MessageReceivedEvent event = request.getEvent();
     if (!channelRepo.hasWritePermission(event.getChannel().getIdLong())) {
       return;
@@ -25,6 +25,7 @@ public class MarkovSelf {
     }
     if (!userRepo.isMarkovCandidate(userid)) {
       MarkovSender.notMarkovCandidate(event.getChannel());
+      return;
     }
     event.getChannel().sendTyping().queue();
     MarkovSender.sendMessageWithDelay(event, Markov.load(userid).generateRandom());
